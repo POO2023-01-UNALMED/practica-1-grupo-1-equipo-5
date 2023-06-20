49e1eca117fea349c0016339f8ba9e5bb020262e
amgg16 <124928492+amgg16@users.noreply.github.com>
2023-06-20 12:28:04 -0500

    Create Ahorro.py

---
+++ python/gestorAplicacion/interfaz/Ahorro.py
@@ -0,0 +1,24 @@
+# Ana Guarín
+# Isabela Hernandez
+# Julián Álvarez
+#Cristian Imbacuan
+
+from gestorAplicacion.interfaz import Cuenta
+from datetime import date
+
+class Ahorro(Cuenta):
+
+    def __init__(self, usuario, nombre, fechaRetiro):
+        super().__init__(usuario, nombre)
+        self._fechaRetiro = fechaRetiro
+
+    def retirar(self, monto):
+        if self._fechaRetiro.isBefore(date.today()):
+            return super().retirar(monto)
+        return False
+
+    def getFechaRetiro(self):
+        return self._fechaRetiro
+
+    def setFechaRetiro(self, fechaRetiro):
+        self._fechaRetiro = fechaRetiro
